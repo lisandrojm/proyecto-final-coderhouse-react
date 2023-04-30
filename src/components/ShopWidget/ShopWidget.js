bc3e46a2fb01c375d389cfa91055d29114241956
lisandrojm <lisandrojm@gmail.com>
2023-04-30 11:11:15 -0300

    del_pe-3

--- src/components/ShopWidget/ShopWidget.js
+++ src/components/ShopWidget/ShopWidget.js
@@ -4,7 +4,7 @@ const ShopWidget = () => {
   return (
     <div className="d-flex align-items-center justify-content-center ">
       <div className="d-flex">
-        <img src={shop} alt="cart-widget" className="img-fluid pe-3" />
+        <img src={shop} alt="cart-widget" className="img-fluid" />
       </div>
     </div>
   );
