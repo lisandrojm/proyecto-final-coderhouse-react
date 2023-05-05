aa7882c87be449a9c0065c6036c553cbc295e613
lisandrojm <lisandrojm@gmail.com>
2023-05-05 16:01:40 -0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import CartProvider from './context/CartContext';
 import Footer from './components/Footer/Footer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
